ca5a743fe0fccda4a62123adcf809b2c40647807
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-01 12:33:53 +0100

    basic structure of file

---
+++ stereo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <map>
+#include <set>
+#include <iomanip>   //setw..
+#include <list>
+#include <forward_list>
+#include <typeinfo>
+#include <string>
+#include <ctime>
+#include <sstream>
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <typeinfo>
+#include <iterator>
+#include <set>
+#include <chrono>
+#include <thread>
+#include <future>
+#include "numbers.h"
+
+using namespace std;
+
+int main(){
+    
+}
