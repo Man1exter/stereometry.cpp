bbee174e2caca4824a2f7b695fb883bf28f6f143
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-01 12:48:46 +0100

    2 blocks(math) and info about

--- stereo.cpp
+++ stereo.cpp
@@ -23,6 +23,39 @@
 
 using namespace std;
 
+void prostopadloscian(){
+    cout << endl;
+    cout << "wzor na objectosc (V = Ppodstawy * H) =====> V = abc" << endl;
+    cout << "pole pow.calkowitej (Pc - suma scian) =======> Pc = 2ab + 2ac + 2bc" << endl;
+    cout << "                inne wazne informacje:     " << endl;
+    cout << "a,b,c -----------> krawedzie prostopadloscianu" << endl;
+    cout << "Dp -------------> przekatna prostopadloscianu" << endl;
+    cout << "Dp ---------------> Pierw.a2 + b2 + c2" << endl;
+    cout << endl;
+}
+
+void szescian(){
+cout << endl;
+    cout << "wzor na objectosc (V = Ppodstawy * H) =====> V = a * a * a" << endl;
+    cout << "pole pow.calkowitej (Pc - suma scian) =======> Pc = 6 * a * a" << endl;
+    cout << "                inne wazne informacje:     " << endl;
+    cout << "a-----------> krawedz szescianu" << endl;
+    cout << "Dsz -------------> przekatna szescianu" << endl;
+    cout << "Dsz ---------------> a pierwiastek 3" << endl;
+    cout << endl;
+}
+
+
 int main(){
-    
+    string bryla;
+
+    cout << "Jaka bryla potrzebna?" << endl;
+    cin >> bryla;
+
+    if(bryla == "prostopadloscian"){
+    prostopadloscian();
+} else if(bryla == "szescian"){
+ szescian();
+}
+
 }
